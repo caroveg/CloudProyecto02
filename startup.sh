f9ba56cab40681c0d711f8f436c93f48d4cadb63
Carolina Castro <caro.castro.v@gmail.com>
2022-04-12 01:29:05 +0000

    Cambio startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PROJECT_BASE_DIR=/home/ubuntu/CloudProyecto02
-source $PROJECT_BASE_DIR/proyecto03v/bin/activate
+. $PROJECT_BASE_DIR/proyecto03v/bin/activate
 cd $PROJECT_BASE_DIR/
-nohup python entrypoint.py 2>&1
+python entrypoint.py
