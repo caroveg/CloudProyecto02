da6f06f2a975b68c2bf2808e304da66ff125b8f3
iaSalazar <ia.salazar@uniandes.edu.co>
2022-05-03 21:35:26 -0500

    .

--- app/__init__.py
+++ app/__init__.py
@@ -22,6 +22,32 @@ dynamodb = boto3.resource('dynamodb',
     region_name='us-east-1'
 )
 
+v_access_key_id='ASIATGWEY6Q5ITLFL2NX'
+v_secret_access_key='qGpA7XkIxVjN7j32MyEfhmvSy04Z1H+LXEZaBf2y'
+v_session_token='FwoGZXIvYXdzELP//////////wEaDCBRzf4DuQB75H2JCiLKAQ8Sx31OTvCu7m8leJ+PJi6mBf5RXC53zdMcxONHrE7hQxWZNspPH0uoVAR3QHwjtwNiFYgfBLd8y+wzWTvL4bSQ6GC8sAu3llSuPBFl55C/9UT/m4Ox1LZC78nua161dfF/pmuKLTkivZilRe3AvrevfQgJPy4rYToxjXHWqhk3uHvUnMrhyX5/urfhsd+NLxcEg2JZ+I4E32YJvnyJgm6IdqLgVhCXpQsdBrTEApHC3VgfVIqbHyZ+uYJvVt2bw7FiDZc9B+aq/oMo66LHkwYyLa8khNk3bY74PxAiddJFq7PDcen1J5S5cJur9PRmPTgAebsNP9cAdtOXXtiUIw=='
+
+dynamodb = boto3.resource('dynamodb',
+    aws_access_key_id=v_access_key_id,
+    aws_secret_access_key=v_secret_access_key,
+	aws_session_token=v_session_token,
+    region_name='us-east-1'
+)
+
+s3 = boto3.resource(
+    service_name='s3',
+    region_name='us-east-1',
+    aws_access_key_id=v_access_key_id,
+    aws_secret_access_key=v_secret_access_key,
+    aws_session_token=v_session_token
+)
+
+
+sqs = boto3.resource('sqs',
+ region_name='us-east-1',
+    aws_access_key_id=v_access_key_id,
+    aws_secret_access_key=v_secret_access_key,
+    aws_session_token=v_session_token)
+
 def create_app():
 
     app = Flask(__name__)
