3a1866bb5b08fbb1728bd75c67b5b568f867a35a
iaSalazar <ia.salazar@uniandes.edu.co>
2022-05-06 10:07:04 -0500

    .

--- wsgi.py
+++ wsgi.py
@@ -7,11 +7,11 @@ from flask import Flask
 cdn = CDN()
 
 
-#app = create_app()
+app = create_app()
 
 if __name__=='__main__':
-    from app import create_app
-    app = create_app()
+    #from app import create_app
+    #app = create_app()
 
     app.config['CDN_DOMAIN'] = 'd25jsbtuwtqsio.cloudfront.net'
     CDN(app)
