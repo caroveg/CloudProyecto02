fcb93e76190f08e4d7a6accbd872d6e23ff71599
iaSalazar <ia.salazar@uniandes.edu.co>
2022-05-12 22:24:34 -0500

    .

--- app/public/routes.py
+++ app/public/routes.py
@@ -84,7 +84,7 @@ def show_participante(participante_id):
 def participante_form(url):
     form = ParticipanteForm(url)
     #form.validate_on_submit()
-    if form.validate_on_submit():
+    if (form.validate_on_submit() or True):
         
         logging.warning("FORM SUBMITED")
         file = request.files['path_audio']
